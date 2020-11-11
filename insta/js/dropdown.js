7c81824f639272bf6303269778b84c4d7c603a21
sksdl9749 <sksdl9749@gmail.com>
2020-11-11 15:26:49 +0900

    5.HTML-CSS 끝

--- insta/js/dropdown.js
+++ insta/js/dropdown.js
@@ -7,14 +7,14 @@ $(".dropdirect").hide();
 $(document).mouseup(function (e) {
     activity.click(function () {
         container.toggle();
-        if ( container.css("display") == "none" ) $("#activity-icon").attr("src", "../images/like_outline.png");
-    else $("#activity-icon").attr("src", "../images/like.png");
+        if ( container.css("display") == "none" ) $("#activity-icon").attr("src", "images/like_outline.png");
+    else $("#activity-icon").attr("src", "images/like.png");
 
     })
     
     if (container.has(e.target).length == 0){
         container.hide();
-        $("#activity-icon").attr("src", "../images/like_outline.png");
+        $("#activity-icon").attr("src", "images/like_outline.png");
     }    
 });
 
