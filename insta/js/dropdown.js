414466f317dc5bc42fdd997da5d7b57f1ab4655c
sksdl9749 <sksdl9749@gmail.com>
2020-11-11 12:12:30 +0900

    4. 인스타그램

--- insta/js/dropdown.js
+++ insta/js/dropdown.js
@@ -1,14 +1,25 @@
+var container = $(".dropdirect");
+var activity = $(".menu__activity");
+
+
 
 $(".dropdirect").hide();
 $(document).mouseup(function (e) {
-
-    var container = $(".dropdirect");
-    var activity = $(".menu__activity");
-
     activity.click(function () {
         container.toggle();
+        if ( container.css("display") == "none" ) $("#activity-icon").attr("src", "../images/like_outline.png");
+    else $("#activity-icon").attr("src", "../images/like.png");
 
     })
-    if (container.has(e.target).length == 0) container.hide();
+    if (container.has(e.target).length == 0){
+        container.hide();
+        $("#activity-icon").attr("src", "../images/like_outline.png");
+    }
+ 
+        // $("#activity-icon").attr("src", "../images/like_outline.png");
+  
+        
+    
+    
 });
 
